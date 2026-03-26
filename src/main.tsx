b33dfa5b525c4e144a46e480fcdb2e66d45882ea
권수빈 <kwonsubin@gwonsubin-ui-MacBookAir.local>
2026-03-27 03:25:10 +0900

    Chore: 메인에 존재하는 Team 관련 로컬 스토리지 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -12,24 +12,17 @@ import { ChatProvider } from "./contexts/ChatContext"
 import { UserProvider } from "./contexts/UserContext"
 import { LogProvider } from "./contexts/LogContext"
 import { DmRequestProvider } from "./contexts/DmRequestContext"
-import teamsData from "./data/team_dummy_data.json"
 import { HACKATHON_DATA_VERSION, normalizedHackathons } from "./lib/hackathonData"
 
 const queryClient = new QueryClient()
 
 const HACKATHONS_STORAGE_KEY = "hackathons"
 const HACKATHONS_VERSION_KEY = "hackathons_data_version"
-const TEAMS_STORAGE_KEY = "teams"
 
 const storedHackathonsVersion = localStorage.getItem(HACKATHONS_VERSION_KEY)
 if (storedHackathonsVersion !== HACKATHON_DATA_VERSION) {
   localStorage.setItem(HACKATHONS_STORAGE_KEY, JSON.stringify(normalizedHackathons))
   localStorage.setItem(HACKATHONS_VERSION_KEY, HACKATHON_DATA_VERSION)
-}
-
-const storedTeams = localStorage.getItem(TEAMS_STORAGE_KEY)
-if (storedTeams === null) {
-  localStorage.setItem(TEAMS_STORAGE_KEY, JSON.stringify(teamsData))
 }
 
 createRoot(document.getElementById("root")!).render(
